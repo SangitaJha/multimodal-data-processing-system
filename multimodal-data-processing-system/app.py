5cba2e559574ed9c69fe30b0a698c44f8450c6a0
Sangita Jha <118240694+SangitaJha@users.noreply.github.com>
2025-10-31 14:44:25 +0530

    Update app.py

--- multimodal-data-processing-system/app.py
+++ multimodal-data-processing-system/app.py
@@ -7,10 +7,10 @@ load_dotenv()
 
 api_key = os.getenv("GEMINI_API_KEY")
 if not api_key:
-    raise EnvironmentError("❌ GEMINI_API_KEY not found! Please set it in your .env file.")
+    raise EnvironmentError(" GEMINI_API_KEY not found! Please set it in your .env file.")
 
 if __name__ == "__main__":
     text = extract_pdf_text("data/pdf/sample.pdf")
     summary = summarize_text(text)
-    print("✅ Summary Generated:")
+    print("Summary Generated:")
     print(summary)
